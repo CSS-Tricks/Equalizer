f26f737e96e5bc95148960fb37be4417a8a45fa2
Rob Garrison <wowmotty@gmail.com>
2012-01-12 16:23:35 -0600

    better support for multiple instances

--- README.markdown
+++ README.markdown
@@ -1,13 +1,13 @@
-##Features ([Demo](http://mottie.github.com/Equalizer/))
+# Features ([Demo](http://mottie.github.com/Equalizer/))
 
 * Equalize column heights across multiple rows (originally from [this post](http://css-tricks.com/8401-equal-height-blocks-in-rows/))
 * Resize column heights when the page is resized. Can be disabled for fixed width layouts.
 * Choose between height (default), outer height or inner height.
 * Set a minimum or maximum height of a column, with an css class added when content overflows for additional styling
 
-##Setup
+# Setup
 
-###Header
+## Header
 
 ```html
 <!-- Required CSS styling, but do whatever you want with the overflow and other styling -->
@@ -20,13 +20,14 @@
 <script src="js/jquery.equalizer.js"></script>
 <script>
 $(function() {
-  $('.wrapper > div').equalizer();
+  $('.wrapper').equalizer();
 });
 </script>
 
 ```
 
-###Example HTML
+## Example HTML
+
 ```html
 <div class="wrapper">
   <div>Block 1</div>
@@ -38,14 +39,17 @@ $(function() {
 </div>
 ```
 
-##Usage & Options (defaults)
+# Usage & Options (defaults)
 
 ```javascript
-$('.wrapper > div').equalizer({
+// target the wrapper of all of the elements to equalize,
+// in this case the divs inside of .wrapper
+$('.wrapper').equalizer({
   // height = type of height to use
   // "o" or "outer" = "outerHeight" - includes height + padding + border + margin
   // "i" or "inner" = "innerHeight" - includes height + padding + border
   // default        = "height"      - use just the height
+  columns    : '> div',     // elements inside of the wrapper
   useHeight  : 'height',    // height measurement to use
   resizeable : true,        // when true, heights are adjusted on window resize
   min        : 0,           // Minimum height applied to all columns
@@ -54,13 +58,42 @@ $('.wrapper > div').equalizer({
 });
 ```
 
-##Change Log
+# Change Log
 
-###Version 1.0.1 (7/14/2011)
+## Version 1.1 (1/12/2012)
+
+* Big change made in the plugin structure to better allow multiple instances.
+  * Changed how the plugin is called. Call the plugin on the wrapper with the jQuery selector within the wrapper. The elements selected by the columns option will be equalized.
+
+		```javascript
+		// old format = $('.wrapper > div')
+		// new format below
+		$('.wrapper').equalizer({
+			// find divs that are the immediate children of the wrapper
+			columns : '> div'
+		});
+		```
+
+  * Added a method to resize the columns
+
+		```javascript
+		// initialize the plugin
+		$('.wrapper').equalizer({ resizable: false });
+
+		// update column heights on button click
+		$('button').click(function(){
+		  // use the data object
+		  $('.wrapper').data('Equalizer').update();
+		  // or call the plugin again, any new options will be ignored
+		  // $('.wrapper').equalizer();
+		});
+		```
+
+## Version 1.0.1 (7/14/2011)
 
 * Fixed a problem with adding headers, and headers with custom fonts, inside the blocks.
 * Added headers into the demo.
 
-###Version 1.0 (6/6/2011)
+## Version 1.0 (6/6/2011)
 
 * Initial build on Github
--- js/jquery.equalizer.js
+++ js/jquery.equalizer.js
@@ -1,5 +1,5 @@
 /*
-	Equalizer v1.0.1
+	Equalizer v1.1
 
 	Original by Chris Coyier: http://css-tricks.com/equal-height-blocks-in-rows/
 	from an idea by Stephen Akins: http://stephenakins.blogspot.com/2011/01/uniform-div-heights-for-liquid-css-p.html
@@ -7,101 +7,127 @@
 	converted into a plugin with some improvements by Rob Garrison: https://github.com/Mottie
 */
 
-(function($) {
-$.fn.equalizer = function(options) {
-	var base = {};
-	$('body').data('Equalizer', base);
-	base.$el = $(this); // base.$el contains all elements
-	base.options = $.extend({}, $.equalizerDefaults, options);
+(function($){
+$.equalizer = function(el, options){
+	var o, base = this;
+	base.$el = $(el);
+	base.$el.data("equalizer", base);
 
 	base.init = function(){
+		base.options = o = $.extend({}, $.equalizer.defaultOptions, options);
+
+		// save columns to equalize
+		base.$col = base.$el.find(o.columns);
+		var t = base.$col.find('.equalizer-inner').length;
+
 		// Setup sizes
-		base.options.min = parseInt(base.options.min, 10) || 0;
-		base.options.max = parseInt(base.options.max, 10) || 0;
-		base.hasMax = (base.options.max === 0) ? false : true;
-		base.hasMin = (base.options.min === 0) ? false : true;
+		o.min = parseInt(o.min, 10) || 0;
+		o.max = parseInt(o.max, 10) || 0;
+		base.hasMax = (o.max === 0) ? false : true;
+		base.hasMin = (o.min === 0) ? false : true;
 		base.curRowTop = 0; // current row offset top position
 
 		// height to use
-		base.useHeight = (/^o/.test(base.options.useHeight)) ? 'outerHeight' : /^i/.test(base.options.useHeight) ? 'innerHeight' : 'height';
+		base.useHeight = (/^o/.test(o.useHeight)) ? 'outerHeight' : /^i/.test(o.useHeight) ? 'innerHeight' : 'height';
 
-		if (base.options.resizeable) {
+		if (!t) {
 			// wrap content with a span so we can always get the exact height of the content on resize
 			// inline styling needed for Chrome; the span must have display:block, or use a div
-			base.$el.wrapInner('<span class="equalizer-inner" style="display:block;margin:0;padding:0;" />');
+			base.$col.wrapInner('<span class="equalizer-inner" style="display:block;margin:0;padding:0;" />');
+		}
 
+		// throttle window resize if option set
+		if (!t && o.resizeable) {
 			// throttled resize columns
 			$(window).resize(function(){
 				clearTimeout(base.throttle);
 				base.throttle = setTimeout(function(){
-					base.equalz();
+					base.update();
 				}, 100);
 			});
 		}
 
-		base.equalz();
+		base.update();
 
 	};
 
-	base.equalz = function(){
-		base.curMax = base.options.min; // current max height
+	base.update = function(){
+		base.curMax = o.min; // current max height
 
-		base.$el
-		.removeClass(base.options.overflow) // removed as it may have changed on resize
+		base.$col
+		.removeClass(o.overflow) // removed as it may have changed on resize
 		.each(function(){
 			var $this = $(this),
-				$el = (base.options.resizeable) ? $this.find('span.equalizer-inner') : $this;
+				$el = $this.find('span.equalizer-inner');
 			// find offset (position relative to document)
 			base.curTop = $this.offset().top;
 
 			// Check for new row
 			if (base.curRowTop !== base.curTop) {
 				// New row, so check for max height first
-				if (base.hasMax && base.curMax > base.options.max) {
-					base.curMax = base.options.max;
-					base.curRows.addClass(base.options.overflow);
+				if (base.hasMax && base.curMax > o.max) {
+					base.curMax = o.max;
+					base.curRows.addClass(o.overflow);
 				}
 				// New row found, set the heights of the previous row
 				// but ignore the row if not defined (very first element)
 				if (base.curRows) { base.curRows.height(base.curMax); }
 				// Set the variables for the new row
 				base.curMax = $el[base.useHeight]();
-				base.curMax = (base.hasMin) ? Math.max(base.options.min, base.curMax) : base.curMax;
+				base.curMax = (base.hasMin) ? Math.max(o.min, base.curMax) : base.curMax;
 				base.curRowTop = base.curTop;
 				base.curRows = $this;
 			} else {
 				// Same row, continue comparing heights
 				base.curMax = Math.max(base.curMax, $el[base.useHeight]());
 				// Check limitations
-				base.curMax = (base.hasMax && base.curMax > base.options.max) ?
-					base.options.max : 
-					(base.hasMin && base.curMax < base.options.min) ? base.options.min : base.curMax;
+				base.curMax = (base.hasMax && base.curMax > o.max) ?
+					o.max :
+					(base.hasMin && base.curMax < o.min) ? o.min : base.curMax;
 				// another div on the current row, add it to the list
 				base.curRows = base.curRows.add($this);
 			}
 			// catch last row
 			if (base.curRows) {
 				base.curRows.height(base.curMax);
-				if (base.hasMax && base.curMax >= base.options.max) {
-					base.curRows.addClass(base.options.overflow);
+				if (base.hasMax && base.curMax >= o.max) {
+					base.curRows.addClass(o.overflow);
 				}
 			}
 		});
 	};
+
 	base.init();
-	return this; // maintain chainability
 };
 
-$.equalizerDefaults = {
+$.equalizer.defaultOptions = {
 	// height = type of height to use
 	// "o" or "outer" = "outerHeight" - includes height + padding + border + margin
 	// "i" or "inner" = "innerHeight" - includes height + padding + border
 	// default        = "height"      - use just the height
-	useHeight : 'height',    // height measurement to use
-	resizeable: true,        // when true, heights are adjusted on window resize
-	min       : 0,           // Minimum height applied to all columns
-	max       : 0,           // Max height applied to all columns
-	overflow  : 'overflowed' // class applied to columns that are taller than the allowable max
+	columns    : '> div',     // elements inside of the wrapper
+	useHeight  : 'height',    // height measurement to use
+	resizeable : true,        // when true, heights are adjusted on window resize
+	min        : 0,           // Minimum height applied to all columns
+	max        : 0,           // Max height applied to all columns
+	overflow   : 'overflowed' // class applied to columns that are taller than the allowable max
+};
+
+$.fn.equalizer = function(options){
+	return this.each(function(){
+		var equalizer = $(this).data('equalizer');
+		// initialize the slider but prevent multiple initializations
+		if (!equalizer) {
+			(new $.equalizer(this, options));
+		} else {
+			equalizer.update();
+		}
+	});
+};
+
+// return the equalizer object
+$.fn.getequalizer = function(){
+	return this.data("equalizer");
 };
 
 })(jQuery);
